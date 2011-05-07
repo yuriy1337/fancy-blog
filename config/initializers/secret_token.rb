bafa5a27d9fbade3e57b27cc769a531294c9a75a
unknown <Yuriy@.(none)>
2011-05-07 14:39:03 -0500

    adding rails generated stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bridalka::Application.config.secret_token = 'ef38a3372a93a4333539d6c5212eded2785aea9b9b06e14e12cc723424512a55a20c281d8392f66eb008ef767317246949f6b1ccadd40165b0148314a5dba288'
