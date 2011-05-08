49bec3e3558dc9c51b736d6b8d4be5ccf98b211a
Yuriy <yuriy1337@gmail.com>
2011-05-08 16:24:38 -0500

    updating schema.rd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110508202857) do
+ActiveRecord::Schema.define(:version => 20110508212305) do
 
   create_table "images", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20110508202857) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "stream_id"
   end
 
   create_table "streams", :force => true do |t|
