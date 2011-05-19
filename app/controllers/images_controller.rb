99b29b2f443fa3a89ef553e8a4e87ef89f1d2a9c
Yuriy <yuriy1337@gmail.com>
2011-05-19 17:45:54 -0500

    got posts to display images & tags to show posts

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -21,7 +21,7 @@ class ImagesController < ApplicationController
   # GET /images/1
   # GET /images/1.xml
   def show
-    @image = @post.images.find(params[:id])
+    @images = @post.images.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
